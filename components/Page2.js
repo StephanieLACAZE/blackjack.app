0759f677ed4d7c6e86141af5fd8a88c6175b703f
Amandine DRIANT <mandine32@gmail.com>
2021-01-20 12:42:02 +0100

    HELP !

--- components/Page2.js
+++ components/Page2.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function Page1(){
     return <View>
         <Text>
-            Page 36000
+            Pages 36000, SOS, c'est trop dur ! 
         </Text>
     </View>
 }

92e2a404c505b1905189740414f8ba03966db9ee
aymone2k <73581149+aymone2k@users.noreply.github.com>
2021-01-20 12:19:19 +0100

    modif coucou header

--- components/Page1.js
+++ components/Page1.js
@@ -5,7 +5,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function Page1(){
     return <View>
         <Text>
-            Page 1
+            COUCOU
         </Text>
     </View>
 }

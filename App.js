2046aba8554895081fef4984425e73148e0456ed
stephanie <s_lacaze@digital-campus.net>
2021-01-20 12:05:33 +0100

    dans App.js, j'ai enlevé Text dans import react-native

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View,TextInput } from 'react-native';
 
 
 
@@ -12,31 +12,73 @@ import Page3 from './components/Page3';
 
 
 
+
 export default function App() {
 
-  const [afficher, setAfficher] = useState(false);
+  const [page, setPage]= useState("");
 
-
-
-  const handlePress = function () {
-    setAfficher(!afficher)
-
+  const handlePress1 =function(){
+    setPage ("page_1")
   }
-
-
+  const handlePress2= function(){
+    setPage ("page_2")
+  }
+  const handlePress3= function() {
+    setPage ("page_3")
+  }
+  const [value, onChangeText]= useState("");
+  const [value2, setValue2]= useState("")
+ 
+  const handlePress= function(){
+        if(value)  value2
+    
+  }  
 
   return (
     <View style={styles.container}>
       <Header title="Blackjack app" />
       <View style={styles.body}>
-        {afficher === true &&
-
-          <Text>Coucou</Text>
-        }
-
-        <Button title={afficher === false ? "Afficher" : "Cacher"} onPress={handlePress} />
+      {page === "page_1" &&
+        <Text>page 1</Text>}
+      {page === "page_2"&&
+       <Text>page 2</Text>}
+      {page === "page_3"&&
+        <Text>page 3</Text>}
+      
+      <Button title = {"Page 1" } onPress={handlePress1}/>
+      <Button title = {"Page 2" } onPress={handlePress2}/>
+      <Button title = {"Page 3" } onPress={handlePress3}/>    
       </View>
       <StatusBar style="auto"/>
-    </View>
 
-  )}
+        <Text>{value}</Text>
+        <TextInput style={styles.subject}
+        onChangeText = {text=>onChangeText(text)}
+        value ={value} />
+        <Button style={styles.bouton} title={"effacer"} onPress={handlePress}/>
+    </View>
+   
+    
+  )
+
+}
+  const styles = StyleSheet.create({
+    body: {
+      color: "red",
+    },
+    subject: {
+      borderColor :"red",
+      borderStyle: "solid",
+      borderwidth: 10,
+      height: 80,
+    },
+    bouton: {
+      margin:10,
+      width:20,
+      heigth :20,
+      backgroundColor:"purple",
+
+    }
+
+    
+  })

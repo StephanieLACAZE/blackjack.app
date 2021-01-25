3a17fcb7359360cf99a434aa736282ab16072786
stephanie <s_lacaze@digital-campus.net>
2021-01-25 11:19:45 +0100

    nouveau App.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet,View, Text, TextInput } from 'react-native';
+import { Button, StyleSheet,View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 
-import Header from './components/Header'
+import Header from './components/Header';
 import Navbar from './components/Navbar'
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
+import Page2 from './components/table';
 
 
 
@@ -15,70 +16,46 @@ import Page2 from './components/Page2';
 
 export default function App() {
 
-  const [page, setPage]= useState("");
+const [titrePage, setTitrePage] = useState("Home");
 
-  const handlePress1 =function(){
-    setPage ("page_1")
-  }
-  const handlePress2= function(){
-    setPage ("page_2")
-  }
-  const handlePress3= function() {
-    setPage ("page_3")
-  }
-  const [value, onChangeText]= useState("");
-  const [value2, setValue2]= useState("")
- 
-  const handlePress= function(){
-        if(value)  value2
-    
-  }  
 
-  return (
-    <View style={styles.container}>
-      <Header title="Blackjack app" />
-      <View style={styles.body}>
-      {page === "page_1" &&
-        <Text>page 1</Text>}
-      {page === "page_2"&&
-       <Text>page 2</Text>}
-      {page === "page_3"&&
-        <Text>page 3</Text>}
-      
-      <Button title = {"Page 1" } onPress={handlePress1}/>
-      <Button title = {"Page 2" } onPress={handlePress2}/>
-      <Button title = {"Page 3" } onPress={handlePress3}/>    
-      </View>
-      <StatusBar style="auto"/>
 
-        <Text>{value}</Text>
-        <TextInput style={styles.subject}
-        onChangeText = {text=>onChangeText(text)}
-        value ={value} />
-        <Button style={styles.bouton} title={"effacer"} onPress={handlePress}/>
-    </View>
-   
-    
-  )
-
+const [page, setPage] =useState("table");
+const handlePress = function (){
+  setPage ("table 1")
 }
-  const styles = StyleSheet.create({
-    body: {
-      color: "red",
-    },
-    subject: {
-      borderColor :"red",
-      borderStyle: "solid",
-      borderwidth: 10,
-      height: 80,
-    },
-    bouton: {
-      margin:10,
-      width:20,
-      heigth :20,
-      backgroundColor:"purple",
 
-    }
 
-    
-  })
+
+
+return (
+<View style ={styles.container}>
+  <Header title = "BLACKJACK APP"/>
+  <Header title = {titrePage}/>
+  <View style = {styles.main}>
+    <View style= {styles.button}>
+      <Button title= "Play!" color = "#11993" onPress={handlePress}/>
+    </View>
+  </View>
+  <StatusBar style ="auto"/>
+  </View>
+
+);
+}
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: "#BBD2E1",
+    alignItems: "center",
+    justifyContent: 'flex-start',
+  },
+  main: {
+    marginTop:20,
+  },
+  button : {
+    margin:20,
+    alignItems: "center",
+  },
+
+  page:{},
+})
